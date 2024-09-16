25e262e1baa3cd6e71244ecd41025cf4ee96a691
gongahkia <angryapplegravy@gmail.com>
2024-09-16 23:59:07 +0800

    added everything

---
+++ logic/README.md
@@ -0,0 +1,7 @@
+# Logic evaluation
+
+* Boolean minimization in `prop_eval.py`
+
+```console
+$ python3 learn.py
+```
--- logic/learn.py
+++ logic/learn.py
@@ -8,14 +8,13 @@ import prop_eval as p
 
 if __name__ == "__main__":
 
-    variables = ["A", "B", "C"]
-    custom_formula = "A ∨ (¬B ∧ C)"
+    variables = ["A", "B", "C", "D"]
+    custom_formula = "(A∧¬B)∨(C⊕D)"
 
     truth_table = p.generate_truth_table(variables)
-    print("Truth Table for Basic Propositional Formulas with Multiple Variables:")
+    print("Truth Table for Basic Propositional Formulas with Multiple Variables")
     p.display_truth_table(truth_table, variables)
-    print("---")
-    print(f"Evaluating custom formula: {custom_formula}")
+    print(f"Evaluating the custom formula {custom_formula}")
     truth_table_custom = p.evaluate_formula(custom_formula, variables)
     p.display_truth_table_custom(truth_table_custom, variables)
     minimized_result = p.minimize_formula(custom_formula, variables)
